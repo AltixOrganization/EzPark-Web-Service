9415b53c48c3026e6434bf06bd5752ec2bd835f8
Dark7YT <u202210846@upc.edu.pe>
2025-05-12 00:59:19 -0500

    first commit

---
+++ src/main/java/com/ezpark/web_service/EzparkApplication.java
@@ -0,0 +1,13 @@
+package com.ezpark.web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezpark/web_service/EzparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezpark.web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
