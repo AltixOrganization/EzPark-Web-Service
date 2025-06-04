7b08d3221d4c5525f6bae91313824caf6a8be399
Michael Carmelino Dueñas <carmelino0213@gmail.com>
2025-06-04 17:22:20 -0500

    update user information

--- src/main/java/com/ezpark/web_service/iam/interfaces/rest/resources/AuthenticatedUserResource.java
+++ src/main/java/com/ezpark/web_service/iam/interfaces/rest/resources/AuthenticatedUserResource.java
@@ -1,4 +1,6 @@
 package com.ezpark.web_service.iam.interfaces.rest.resources;
 
-public record AuthenticatedUserResource(Long id, String email, String token) {
+import java.util.List;
+
+public record AuthenticatedUserResource(Long id, String email, String token, List<String> roles) {
 }
--- src/main/java/com/ezpark/web_service/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
+++ src/main/java/com/ezpark/web_service/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
@@ -1,11 +1,15 @@
 package com.ezpark.web_service.iam.interfaces.rest.transform;
 
 import com.ezpark.web_service.iam.domain.model.aggregates.User;
+import com.ezpark.web_service.iam.domain.model.entities.Role;
 import com.ezpark.web_service.iam.interfaces.rest.resources.AuthenticatedUserResource;
 
 public class AuthenticatedUserResourceFromEntityAssembler {
 
   public static AuthenticatedUserResource toResourceFromEntity(User user, String token) {
-    return new AuthenticatedUserResource(user.getId(), user.getEmail(), token);
+    var roles = user.getRoles().stream()
+            .map(Role::getStringName)
+            .toList();
+    return new AuthenticatedUserResource(user.getId(), user.getEmail(), token, roles);
   }
 }
